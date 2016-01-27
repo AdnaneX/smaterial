af14f1f0e0631e27e023fc948985eff006e08786
SuperDJ <super.derkjan@gmail.com>
2016-01-27 20:55:16 +0100

    Fixed appbar name, changed font colors, made additons to drawer

--- index.php
+++ index.php
@@ -14,17 +14,17 @@
 	</head>
 
 	<body>
-	 	<header class="sm-navbar">
-			<div class="sm-navbar-nav">
+	 	<header class="sm-appbar">
+			<div class="sm-appbar-nav">
 				<a href="#" id="sm-nav-button"><i class="material-icons">menu</i></a>
 			</div>
 
-			<div class="sm-navbar-title">
+			<div class="sm-appbar-title">
 				<h1>Home</h1>
 			</div>
 
-			<div class="sm-navbar-actions">
-				<div class="sm-navbar-search">
+			<div class="sm-appbar-actions">
+				<div class="sm-appbar-search">
 					<a href="#"<i class="material-icons">search</i></a>
 
 					<form action="" method="post">
@@ -33,7 +33,7 @@
 				</div>
 			</div>
 
-			<div class="sm-navbar-menu">
+			<div class="sm-appbar-menu">
 				<a href="#" id="sm-menu-button" class="sm-dropdown" data-sm-trigger="sm-nav-more"><i class="material-icons">more_vert</i></a>
 
 				<ul id="sm-nav-more">
@@ -43,13 +43,20 @@
 		</header>
 
 		<aside>
-			<nav id="sm-sidenav" class="sm-sidenav sm-sidenav-left sm-sidenav-floating">
+			<nav id="sm-sidenav" class="sm-sidenav sm-sidenav-left sm-sidenav-left-floating sm-sidenav-dropdown">
 				<ul>
-					<li class="sm-sidenav-profile">
-
+					<li class="sm-sidenav-drawer">
+						<img alt="thumbnail" src="/images/thumb.jpg" class="sm-circle sm-sidenav-drawer-img">
 					</li>
-					<li><a href="">Example</a></li>
-					<li><a href="">Example 2</a></li>
+					<li><a href="/"><i class="material-icons">home</i> Home</a></li>
+					<li>Components
+						<ul>
+							<li><a href="colors.php"> <span>Colors</span></a></li>
+							<li><a href="typography.php">Typography</a></li>
+							<li><a href="appbar.php">Appbar</a></li>
+							<li><a href="sidenav.php">Sidenav</a></li>
+						</ul>
+					</li>
 				</ul>
 			</nav>
 		</aside>
