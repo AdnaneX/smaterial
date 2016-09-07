75a5caeea47928d610052c9de0e30da101203749
SuperDJ <super.derkjan@gmail.com>
2016-09-07 20:33:08 +0200

    Redoing cards, started creating card templates

--- components/cards.php
+++ components/cards.php
@@ -5,34 +5,68 @@ $keywords = 'material design, smaterial, cards';
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/includes/header.php';
 ?>
+<section class="sc-row">
+	<h2>Cards usage</h2>
 
-	<section class="sc-row">
-		<h2>Cards usage</h2>
+	<p class="sc-col sc-xs4 sc-s12 sc-m6">
+		There are many different variations of cards.
+		You can make each card with the blocks provided.
+		Each block has sub classes to follow the design specs.
+	</p>
+</section>
 
-		<p class="sc-col sc-xs4 sc-s12 sc-m6">
-			There are many different variations of cards.
-			Below are example cards.
-		</p>
-	</section>
+<section class="sc-row">
+	<h3>Image with text</h3>
 
-	<section class="sc-row">
-		<h3>Card structures</h3>
+	<p class="sc-col sc-xs4 sc-s12 sc-m6">
+		A very basic card with just an image and text.
+		The <code class="language-css">.sc-card-top</code> class makes sure the image gets rounded edges.
+	</p>
 
-		<div class="sc-col sc-xs4 sc-s12 sc-m6 sc-l3">
-			<div class="sc-card sc-card-basic">
-				<div class="sc-card-basic-title">
-					<div class="sc-card-title-primary sc-large">Example primary</div>
-					<div class="sc-card-title-sub">Example sub title</div>
-				</div>
+	<div class="sc-col sc-xs4 sc-s12 sc-m2">
+		<div class="sc-card">
+			<img src="/images/album-example.jpg" class="sc-card-top">
 
-				<div class="sc-card-content">
-					This is some content text
-				</div>
-
-				<div class="sc-card-actions sc-card-divider"></div>
+			<div class="sc-card-supporting">
+				This is supporting text.
 			</div>
 		</div>
-	</section>
+	</div>
 
+	<pre class="language-html sc-col sc-xs4 sc-s12 sc-m6 sc-offset-m6">
+		<code>
+&lt;div class="sc-card">
+  &lt;img src="/images/album-example.jpg" class="sc-card-top">
+
+  &lt;div class="sc-card-supporting">
+    This is supporting text.
+  &lt;/div>
+&lt;/div>
+		</code>
+	</pre>
+</section>
+
+<section class="sc-row">
+	<h3>Avatar, title and subtitle</h3>
+
+	<div class="sc-col sc-xs4 sc-s12 sc-m3">
+		<div class="sc-card">
+			<div class="sc-card-primary sc-card-primary-small sc-card-primary-additional">
+				This is a title
+				<div class="sc-subhead-1">Subhead</div>
+			</div>
+			<img src="/images/album-example.jpg">
+
+			<div class="sc-card-supporting sc-card-supporting-additional">
+				This is supporting text.
+			</div>
+
+			<div class="sc-card-actions">
+				<button class="sc-flat-button">Action 1</button>
+				<button class="sc-flat-button">Action 2</button>
+			</div>
+		</div>
+	</div>
+</section>
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php';
