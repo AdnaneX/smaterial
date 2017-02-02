81831eb178532bb808858193f947f7929795afcc
SuperDJ <super.derkjan@gmail.com>
2017-02-02 15:20:03 +0100

    Fixed a few minor bugs

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task( 'watch', function () {
 	gulp.watch( 'js/app/*.js', ['app'] );
 	gulp.watch( 'js/vendor/*.js', ['vendor'] );
 	gulp.watch( 'stylesheets/scss/*.scss', ['sass'] );
-	gulp.watch( 'stylesheets/scss/components/*.scss', ['sass'] );
+	gulp.watch( 'stylesheets/scss/**/*.scss', ['sass'] );
 } );
 
 gulp.task( 'default', ['watch'] );
